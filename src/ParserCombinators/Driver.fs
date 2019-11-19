ef8e3961c57afe09a8d7476ff729a6b45032641f
bagelhobbit <evanturner45@gmail.com>
2019-11-18 21:07:05 -0700

    Add closing hash and trim strings
    still doesn't properly handle escaped hashes

--- src/ParserCombinators/Driver.fs
+++ src/ParserCombinators/Driver.fs
@@ -69,30 +69,49 @@ let main args =
 
 """
 
+    let charToStr c = sprintf "%c" c
+
+    let stringListToString (strings:string list) = 
+        strings
+        |> List.reduce (+)
+
     let textP = 
         let text = satisfy (fun c -> c <> '\n') "text" 
         // Only parse a single line
         many text .>> pchar '\n' |>> charListToStr |>> Text <?> "text"
 
-    let atxHeadingP = 
+    let atxHeadingP =
         let hash = pchar '#'
-        let heading = satisfy (fun ch -> ch <> '\n') "text"
-
         let space = pchar ' '
 
-        // Parse and ignore the trailing newline so the parser moves past it.
-        let headingContent = many1 space >>. many heading .>> pchar '\n' |>> charListToStr
-
-        let contentOrNewline = headingContent <|> (pstring "\n" >>. returnP "")
+        let contentOrNewline =
+            let heading = 
+                let basicText = satisfy (fun ch -> ch <> '\n' && ch <> '#') "text" |>> charToStr
+                let escapedHash = pstring "\\#" //.>>. many (pstring "#")
+                escapedHash <|> basicText
+            let headingContent = many1 space >>. many heading |>> stringListToString
+            headingContent <|> (pstring "\n" >>. returnP "")
 
         let upTo3Spaces = opt space .>>. opt space .>>. opt space
 
-        let h1 = upTo3Spaces >>. hash >>. contentOrNewline |>> ((fun s -> s.Trim()) >> H1) <?> "h1"
-        let h2 = upTo3Spaces >>. hash >>. hash >>. contentOrNewline |>> H2 <?> "h2"
-        let h3 = upTo3Spaces >>. hash >>. hash >>. hash >>. contentOrNewline |>> H3 <?> "h3"
-        let h4 = upTo3Spaces >>. hash >>. hash >>. hash >>. hash >>. contentOrNewline |>> H4 <?> "h4"
-        let h5 = upTo3Spaces >>. hash >>. hash >>. hash >>. hash >>. hash >>. contentOrNewline |>> H5 <?> "h5"
-        let h6 = upTo3Spaces >>. hash >>. hash >>. hash >>. hash >>. hash >>. hash >>. contentOrNewline |>> H6 <?> "h6"
+        // ATX headers can be optionally closed by any number of hashes
+        let closingHashes = 
+            let endingHashes = many space .>> many hash .>> many space 
+            let trailingSpaces = many space
+            (endingHashes <|> trailingSpaces) .>> pchar '\n'
+
+        let trim (s:string) = s.Trim()
+
+        let h1 = upTo3Spaces >>. hash >>. contentOrNewline .>> closingHashes |>> (trim >> H1) <?> "h1"
+        let h2 = upTo3Spaces >>. hash >>. hash >>. contentOrNewline .>> closingHashes |>> (trim >> H2) <?> "h2"
+        let h3 = upTo3Spaces >>. hash >>. hash >>. hash >>. contentOrNewline .>> closingHashes |>> (trim >> H3) <?> "h3"
+        let h4 = upTo3Spaces >>. hash >>. hash >>. hash >>. hash >>. contentOrNewline .>> closingHashes |>> (trim >> H4) <?> "h4"
+        let h5 = upTo3Spaces >>. hash >>. hash >>. hash >>. hash >>. hash >>. contentOrNewline .>> closingHashes |>> (trim >> H5) <?> "h5"
+        let h6 = upTo3Spaces >>. hash >>. hash >>. hash >>. hash >>. hash >>. hash >>. contentOrNewline .>> closingHashes |>> (trim >> H6) <?> "h6"
+
+        let test = run h3 "### Header endings ##     "
+        printResult test
+
         h1 <|> h2 <|> h3 <|> h4 <|> h5 <|> h6 |>> Header <|> textP
 
     let headingResult = run (many1 atxHeadingP <?> "heading text") input
